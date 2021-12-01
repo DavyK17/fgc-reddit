d761994a3f528259f51b2d2426abdd367bfa33ce
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-02 02:30:46 +0300

    Adding header component

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "../logo.svg";
+
+const Header = () => {
+    return (
+        <header>
+            <div className="col logo">
+                <img src={logo} alt="FGC Reddit" />
+            </div>
+            <div className="col search">
+                <form id="search-bar">
+                    <input type="search" />
+                    <button type="submit">Search</button>
+                </form>
+            </div>
+            <div className="col userbox">
+                <p id="reddit-username">Username</p>
+                <svg id="top-menu" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"/></svg>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
