d6df16f0911694b70013b5eabed4f0a2d10520ae
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-03 01:01:38 +0300

    Passing username through props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "../logo.svg";
 
-const Header = () => {
+const Header = props => {
     return (
         <header>
             <div className="col logo">
@@ -14,7 +14,7 @@ const Header = () => {
                 </form>
             </div>
             <div className="col userbox">
-                <p id="reddit-username">Username</p>
+                <p id="reddit-username">{props.username}</p>
                 <svg id="top-menu" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"/></svg>
             </div>
         </header>
