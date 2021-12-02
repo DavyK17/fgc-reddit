ab944096ceab0966ba7d6df35011a9feeefc77ac
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-02 20:03:42 +0300

    Adding random string generator

---
+++ src/util/randomString.js
@@ -0,0 +1,15 @@
+/*
+Generates a random string of any length of characters
+Source: https://stackoverflow.com/a/1349426/14057610
+*/
+
+export default function makeID(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result;
+}
