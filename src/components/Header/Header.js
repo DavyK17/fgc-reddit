60f462d4e5f16fbaca9dbd06a6ce637f785e4fb3
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-03 02:19:46 +0300

    Importing Username as separate component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,8 @@
-import React, { useState } from "react";
-import Reddit from "../../api/Reddit";
+import React from "react";
 import logo from "../../logo.svg";
+import Username from "./Username";
 
 const Header = () => {
-    const [userID, setUserID] = useState("");
-    Reddit.getCurrentUserId().then(val => {
-      setUserID(val);
-    })
-
     return (
         <header>
             <div className="col logo">
@@ -20,7 +15,7 @@ const Header = () => {
                 </form>
             </div>
             <div className="col userbox">
-                <a id="reddit-username" target="_blank" href={`https://www.reddit.com/user/${userID}`}>{userID}</a>
+                <Username />
                 <svg id="top-menu" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"/></svg>
             </div>
         </header>
