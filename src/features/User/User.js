837c2a7982fc2d1259ee59ecc5a27761b111336a
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-03 17:45:10 +0300

    Adding user slice

---
+++ src/features/User/User.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectUsername } from "./userSlice";
+import Reddit from "../../api/Reddit";
+
+const Username = () => {
+    const userID = useSelector(selectUsername);
+    const { authState, isLoading } = useSelector(state => state.user)
+
+    console.log(userID);
+
+    const handleAuth = (e) => {
+        e.preventDefault();
+        Reddit.getAccessToken();
+    }
+
+    if (isLoading) {
+        return (
+            <svg className="spinner" viewBox="0 0 50 50">
+              <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+            </svg>
+        )
+    } else if (authState) {
+        return (
+            <a id="reddit-username" target="_blank" rel="noreferrer" href={`https://www.reddit.com/user/${userID}`} aria-label="Current user ID">{userID}</a>
+        )
+    } else {
+        return (
+            <a id="reddit-username" href="/" onClick={handleAuth} aria-label="Sign in">Sign in {authState}</a>
+        )
+    }
+}
+
+export default Username;
---
+++ src/features/User/userSlice.js
@@ -0,0 +1,51 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import Reddit from "../../api/Reddit";
+
+export const fetchUser = createAsyncThunk("user/fetchUser", async () => {
+    const token = await Reddit.getAccessToken().then(val => {
+        return val;
+    })
+    const url = "https://oauth.reddit.com/api/v1/me"
+    const headers = {
+        "Authorization": `Bearer ${token}`,
+        "User-Agent": "fgc-reddit by u/DavyK17_ (Codecademy portfolio project)",
+    };
+
+    const response = await fetch(url, { headers: headers });
+    if (response.ok) {
+        const jsonResponse = await response.json();
+        return jsonResponse.name;
+    }
+})
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState: {
+        name: "",
+        authState: false,
+        isLoading: false,
+        hasError: false,
+    },
+    reducers: {},
+    extraReducers: {
+        [fetchUser.pending]: (state, action) => {
+            state.authState = false;
+            state.isLoading = true;
+            state.hasError = false;
+        },
+        [fetchUser.fulfilled]: (state, action) => {
+            state.authState = true;
+            state.isLoading = false;
+            state.hasError = false;
+            state.name = action.payload;
+        },
+        [fetchUser.rejected]: (state, action) => {
+            state.authState = false;
+            state.isLoading = false;
+            state.hasError = true;
+        },
+    }
+});
+
+export const selectUsername = state => state.user.name;
+export default userSlice.reducer;
