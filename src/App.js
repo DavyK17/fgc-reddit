b045ca5436c499699e1586c7f7deadaeb92c4076
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-03 17:47:50 +0300

    Updating for Redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import Header from "./components/Header/Header";
+import { fetchUser } from "./features/User/userSlice";
 import "./util/reset.css";
 import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const stateMatch = window.location.href.match(/state=([^&]*)/);
+    const codeMatch = window.location.href.match(/code=([^&]*)/);
+  
+    if (stateMatch && codeMatch) {
+      dispatch(fetchUser());
+    }
+  })
+
   return (
     <Header />
   );
