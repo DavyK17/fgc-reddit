4a39072c1933e955d8b86898ebbb89e51a97aeec
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-05 15:06:37 +0300

    Setting width and height for #top-menu in css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
             </div>
             <div className="col userbox">
                 <Username />
-                <svg id="top-menu" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"/></svg>
+                <svg id="top-menu" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"/></svg>
             </div>
         </header>
     )
