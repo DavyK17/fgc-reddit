e7279112915d603cc8c54b61c09d1c9b48bdc94e
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-05 15:12:12 +0300

    Editing indentation

--- src/app/store.js
+++ src/app/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from "../features/User/userSlice";
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+    reducer: {
+        user: userReducer,
+    },
 });
