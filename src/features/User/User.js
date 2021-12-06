7293f392493fb250613974da778624aedda47295
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-06 22:28:50 +0300

    Fixing JSX for SVG circle

--- src/features/User/User.js
+++ src/features/User/User.js
@@ -16,7 +16,7 @@ const User = () => {
     if (isLoading) {
         return (
             <svg className="spinner" viewBox="0 0 50 50">
-              <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+              <circle className="path" cx="25" cy="25" r="20" fill="none" strokeWidth="5"></circle>
             </svg>
         )
     } else if (authState) {
