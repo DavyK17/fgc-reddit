90e9101ce861b404eaf4735b1f9d25184b4087e0
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-07 00:29:52 +0300

    Adding test id to SVG spinner

--- src/features/User/User.js
+++ src/features/User/User.js
@@ -10,7 +10,7 @@ const User = props => {
 
     if (isLoading) {
         return (
-            <svg className="spinner" viewBox="0 0 50 50">
+            <svg className="spinner" viewBox="0 0 50 50" data-testid="loading">
               <circle className="path" cx="25" cy="25" r="20" fill="none" strokeWidth="5"></circle>
             </svg>
         )
