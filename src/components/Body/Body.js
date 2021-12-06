74940cfb3cdb91b22aabdb33d54f0f7016b734fe
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-06 22:26:04 +0300

    Creating body file

---
+++ src/components/Body/Body.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import Subs from "./Subs";
+import Sidebar from "./Sidebar";
+
+const Body = () => {
+    return (
+      <section className="reddit">
+          {/* <Subs /> */}
+          <Sidebar />
+      </section>
+    );
+}
+
+export default Body;
