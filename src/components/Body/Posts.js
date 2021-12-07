3a4bc3a12afa0625c2f6792c7e45431134b14f81
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-07 23:02:41 +0300

    Displaying current subreddit in title

--- src/components/Body/Posts.js
+++ src/components/Body/Posts.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
 import Post from "./Post";
+import { selectActive } from "../../features/Subreddits/subredditsSlice";
 
 const Posts = () => {
+    const activeSub = useSelector(selectActive);
+    
     return (
         <main>
             <div className="posts">
-                <h2 id="subreddit-title">{"r/Subreddit"}</h2>
+                <h2 id="subreddit-title">{activeSub.title || activeSub.display_name_prefixed || "r/Subreddit"}</h2>
                 <Post />
             </div>
         </main>
