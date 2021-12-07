620dcec0ca12c9b46944d4626680148f651b8099
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-07 18:26:39 +0300

    Initialising Posts component

--- src/components/Body/Posts.js
+++ src/components/Body/Posts.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Post from "./Post";
 
 const Posts = () => {
-    return;
+    return (
+        <main>
+            <div className="posts">
+                <h2 id="subreddit-title">{"r/Subreddit"}</h2>
+                <Post />
+            </div>
+        </main>
+    );
 }
 
 export default Posts;
