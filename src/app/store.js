af0d045b0ae5951ee6a259893fd535303f12847e
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-07 20:48:18 +0300

    Adding subreddits reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from "../features/User/userSlice";
+import subredditsReducer from "../features/Subreddits/subredditsSlice";
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
+        subreddits: subredditsReducer,
     },
 });
