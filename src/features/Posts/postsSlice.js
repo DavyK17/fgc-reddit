771dbbad213cfb9d8495697e65cd28e58ef1233b
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-08 08:32:00 +0300

    Creating posts slice

---
+++ src/features/Posts/postsSlice.js
@@ -0,0 +1,49 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import Reddit from "../../api/Reddit";
+
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async (name, filter = "hot") => {
+    const data = await Reddit.getPosts(name, filter).then(val => {
+        return val;
+    });
+
+    return data;
+});
+
+export const postsSlice = createSlice({
+    name: "posts",
+    initialState: {
+        posts: [],
+        isLoading: false,
+        hasError: false,
+        searchTerm: "",
+    },
+    reducers: {
+        setPosts: (state, action) => {
+            state.posts = action.payload;
+        },
+        setSearchTerm: (state, action) => {
+            state.searchTerm = action.payload;
+        },
+    },
+    extraReducers: {
+        [fetchPosts.pending]: (state) => {
+            state.isLoading = true;
+            state.hasError = false;
+        },
+        [fetchPosts.fulfilled]: (state, action) => {
+            state.isLoading = false;
+            state.hasError = false;
+            state.posts = action.payload;
+            state.searchTerm = "";
+        },
+        [fetchPosts.rejected]: (state) => {
+            state.isLoading = false;
+            state.hasError = true;
+        },
+    }
+});
+
+export const selectPosts = state => state.posts.posts;
+export const selectSearchTerm = state => state.posts.searchTerm;
+export const { setPosts, setSearchTerm } = postsSlice.actions;
+export default postsSlice.reducer;
