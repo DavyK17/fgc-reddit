64693eb3fab8fa5ec213a42f1dd5e3d40d184238
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-08 08:32:08 +0300

    Adding posts reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from "../features/User/userSlice";
 import subredditsReducer from "../features/Subreddits/subredditsSlice";
+import postsReducer from "../features/Posts/postsSlice";
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
         subreddits: subredditsReducer,
+        posts: postsReducer,
     },
 });
