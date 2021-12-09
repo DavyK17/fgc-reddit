8f302f383a1c574bab6d15aa7355e314d64f4245
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-09 17:55:18 +0300

    Adding post filter logic + created prop for posts

--- src/features/Posts/Posts.js
+++ src/features/Posts/Posts.js
@@ -17,7 +17,7 @@ const Posts = () => {
         if (activeSub.display_name) {
             dispatch(fetchPosts({ name: activeSub.display_name, filter: filter }));
         }
-    }, [dispatch, activeSub.display_name])
+    }, [dispatch, activeSub.display_name, filter])
 
     const title = (
         <h2 id="subreddit-title">{activeSub.title || activeSub.display_name_prefixed || "r/Subreddit"}</h2>
@@ -47,7 +47,7 @@ const Posts = () => {
             {
                 posts.map((s, i) => {
                     return (
-                        <Post key={i} id={s.id} url={s.url} url_overridden_by_dest={s.url_overridden_by_dest} permalink={`https://www.reddit.com${s.permalink}`} post_hint={s.post_hint} score={s.score} author={s.author} title={s.title} num_comments={s.num_comments} selftext_html={s.selftext_html} is_video={s.is_video} secure_media={s.secure_media} secure_media_embed={s.secure_media_embed} />
+                        <Post key={i} id={s.id} created={s.created} url={s.url} url_overridden_by_dest={s.url_overridden_by_dest} permalink={`https://www.reddit.com${s.permalink}`} post_hint={s.post_hint} score={s.score} author={s.author} title={s.title} num_comments={s.num_comments} selftext_html={s.selftext_html} is_video={s.is_video} secure_media={s.secure_media} secure_media_embed={s.secure_media_embed} />
                     )
                 })
             }
