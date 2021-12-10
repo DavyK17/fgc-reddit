32ed847ca1d91ead7762fccb5b00e8316a6ae964
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-10 13:48:41 +0300

    Fleshing out Comment component

--- src/features/Comments/Comment.js
+++ src/features/Comments/Comment.js
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import $ from "jquery";
 
-const Comment = () => {
+import { selectPosts } from "../../features/Posts/postsSlice";
+import epochFromNow from "../../util/epochFromNow";
+
+const Comment = props => {
+    const dispatch = useDispatch();
+
+    const posts = useSelector(selectPosts);
+    const { isLoading } = useSelector(state => state.posts);
+
+    const processHTML = (id, html) => {
+        var txt = document.createElement("textarea");
+        txt.innerHTML = html;
+
+        const container = document.getElementById(`comment-text-${id}`);
+        if ((!isLoading && posts.length !== 0) && container) {
+            if (!container.innerHTML) {
+                container.insertAdjacentHTML("afterbegin", `${txt.value}`);
+            }
+        }
+    }
+
+    useEffect(() => {
+        processHTML(props.id, props.body);
+    }, [dispatch])
+
     return (
         <div className="comment">
             <div className="comment-details-container">
                 <div className="author-details">
                     {/* <img className="author-img" src="gaming.png" alt="Comment author" /> */}
-                    <span className="author-name">Author</span>
+                    <span className="author-name">{props.author}</span>
                 </div>
-                <span className="comment-time mobile-only">8h</span>
-                <span className="comment-time lg-only">8 hours ago</span>
+                <span className="comment-time">{epochFromNow(props.created)}</span>
             </div>
-            <div className="comment-content">
-                <p>Phasellus luctus diam in lobortis scelerisque. Donec pellentesque, purus ut posuere rhoncus, lacus ligula volutpat ante, a luctus tellus dolor quis justo. Morbi elit nisl, fermentum a pretium et, varius ac metus. Mauris
-                    tempus velit ut venenatis luctus. Donec vehicula mauris at dictum pharetra. Etiam semper libero ac felis semper, a laoreet augue congue. Sed consectetur dolor sed lectus varius pellentesque id et sapien.</p>
-            </div>
+            <div className="comment-content" id={`comment-text-${props.id}`}></div>
         </div>
     )
 }
