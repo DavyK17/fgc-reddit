4f2cee3fd8102897dd85dab0d671c89edc926bf0
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-11 01:41:14 +0300

    Adding post IDs to test IDs

--- src/features/Posts/UpDownVote.js
+++ src/features/Posts/UpDownVote.js
@@ -5,11 +5,11 @@ const UpDownVote = props => {
 
     return (
         <div className="post-votes-container">
-            <svg className="vote-button" id={`up-vote-${props.id}`} data-testid="up-vote" aria-label="Up vote" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={handleVote}>
+            <svg className="vote-button" id={`up-vote-${props.id}`} data-testid={`up-vote-${props.id}`} aria-label="Up vote" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={handleVote}>
                 <path d="M0 16.67l2.829 2.83 9.175-9.339 9.167 9.339 2.829-2.83-11.996-12.17z"/>
             </svg>
             <span className="votes-count" id={`vote-count-${props.id}`}>{displayScore}</span>
-            <svg className="vote-button" id={`down-vote-${props.id}`} data-testid="down-vote" aria-label="Down vote" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={handleVote}>
+            <svg className="vote-button" id={`down-vote-${props.id}`} data-testid={`down-vote-${props.id}`} aria-label="Down vote" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={handleVote}>
                 <path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/>
             </svg>
         </div>
