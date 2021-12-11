89a41e3be33e625ceafc318953ace9b2debf3020
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-11 23:13:36 +0300

    Adding test id to logout button

--- src/components/Body/Sidebar.js
+++ src/components/Body/Sidebar.js
@@ -39,7 +39,7 @@ const Sidebar = (props) => {
                     <div className="sidebar-header">
                         <h2 id="sidebar-title">Subreddits</h2>
                         <div className="sidebar-close-container">
-                            <svg id="reddit-logout" className="mobile-only" aria-label="Logout" onClick={handleLogout} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <svg className="mobile-only" id="reddit-logout" data-testid="reddit-logout" aria-label="Logout" onClick={handleLogout} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                 <path d="M0 2v20h14v-2h-12v-16h12v-2h-14zm18 7.408l2.963 2.592-2.963 2.592v-1.592h-8v-2h8v-1.592zm-2-4.408v4h-8v6h8v4l8-7-8-7z"/>
                             </svg>
                             <svg className="sidebar-close" id="sidebar-close" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={closeSidebar}>
--- src/features/User/User.js
+++ src/features/User/User.js
@@ -26,7 +26,7 @@ const User = (props) => {
                   >
                     {userID}
                   </a>
-                  <svg id="reddit-logout" className="lg-only" aria-label="Logout" onClick={handleLogout} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <svg id="reddit-logout" data-testid="reddit-logout" className="lg-only" aria-label="Logout" onClick={handleLogout} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                       <path d="M0 2v20h14v-2h-12v-16h12v-2h-14zm18 7.408l2.963 2.592-2.963 2.592v-1.592h-8v-2h8v-1.592zm-2-4.408v4h-8v6h8v4l8-7-8-7z"/>
                   </svg>
               </Fragment>
