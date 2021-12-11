427f4b132135d0212f50e1f7199db0c63e36f3c3
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-11 23:04:59 +0300

    Using root reducer for store with logout logic

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from "../features/User/userSlice";
 import subredditsReducer from "../features/Subreddits/subredditsSlice";
 import postsReducer from "../features/Posts/postsSlice";
 
+export const appReducer = combineReducers({
+    user: userReducer,
+    subreddits: subredditsReducer,
+    posts: postsReducer,
+});
+
+export const rootReducer = (state, action) => {
+    if (action.type === "logoutUser") {
+        return appReducer(undefined, action);
+    }
+
+    return appReducer(state, action);
+};
+
 export const store = configureStore({
-    reducer: {
-        user: userReducer,
-        subreddits: subredditsReducer,
-        posts: postsReducer,
-    },
+    reducer: rootReducer
 });
