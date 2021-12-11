e4b95e3abd8fe8ea943c2db4a04cb5eda63b7e50
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-12-11 17:51:28 +0800

    added logout method, thunk, and button to clear localstorage and reset to initial state

--- src/features/User/userSlice.js
+++ src/features/User/userSlice.js
@@ -1,42 +1,56 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import Reddit from "../../api/Reddit";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import Reddit from '../../api/Reddit';
 
-export const fetchUser = createAsyncThunk("user/fetchUser", async () => {
-    const user = await Reddit.getUser().then(val => {
-        return val;
-    });
+export const fetchUser = createAsyncThunk('user/fetchUser', async () => {
+  const user = await Reddit.getUser().then((val) => {
+    return val;
+  });
 
-    return user;
-})
-
-export const userSlice = createSlice({
-    name: "user",
-    initialState: {
-        name: "",
-        authState: false,
-        isLoading: false,
-        hasError: false,
-    },
-    extraReducers: {
-        [fetchUser.pending]: (state) => {
-            state.authState = false;
-            state.isLoading = true;
-            state.hasError = false;
-        },
-        [fetchUser.fulfilled]: (state, action) => {
-            state.authState = true;
-            state.isLoading = false;
-            state.hasError = false;
-            state.name = action.payload;
-        },
-        [fetchUser.rejected]: (state) => {
-            state.authState = false;
-            state.isLoading = false;
-            state.hasError = true;
-        },
-    }
+  return user;
 });
 
-export const selectUsername = state => state.user.name;
-export const selectAuthState = state => state.user.authState;
+// logout the user in a hacky way because we're not using react-router
+// so we'll just redirect back to the home page for now
+export const logoutUser = createAsyncThunk('user/logoutUser', async () => {
+  await Reddit.logoutUser();
+  window.location.replace('/');
+});
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    name: '',
+    authState: false,
+    isLoading: false,
+    hasError: false,
+  },
+  extraReducers: {
+    [fetchUser.pending]: (state) => {
+      state.authState = false;
+      state.isLoading = true;
+      state.hasError = false;
+    },
+    [fetchUser.fulfilled]: (state, action) => {
+      state.authState = true;
+      state.isLoading = false;
+      state.hasError = false;
+      state.name = action.payload;
+    },
+    [fetchUser.rejected]: (state) => {
+      state.authState = false;
+      state.isLoading = false;
+      state.hasError = true;
+    },
+    // back to initial state
+    [logoutUser.fulfilled]: (state) => {
+      state.name = '';
+      state.authState = false;
+      state.isLoading = false;
+      state.hasError = false;
+    },
+  },
+});
+
+export const selectUsername = (state) => state.user.name;
+export const selectAuthState = (state) => state.user.authState;
 export default userSlice.reducer;
