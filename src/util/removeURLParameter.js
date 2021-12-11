1a0c3e1df9777f464725abc804a675a84727fff7
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-12 00:43:29 +0300

    Adding URL parameter remover

---
+++ src/util/removeURLParameter.js
@@ -0,0 +1,25 @@
+/*
+    Removes search parameters from URL
+    Source: https://stackoverflow.com/a/1634841/14057610
+*/
+
+export default function removeURLParameter(url, parameter) {
+    //prefer to use l.search if you have a location/link object
+    var urlparts = url.split('?');   
+    if (urlparts.length >= 2) {
+
+        var prefix = encodeURIComponent(parameter) + '=';
+        var pars = urlparts[1].split(/[&;]/g);
+
+        //reverse iteration as may be destructive
+        for (var i = pars.length; i-- > 0;) {    
+            //idiom for string.startsWith
+            if (pars[i].lastIndexOf(prefix, 0) !== -1) {  
+                pars.splice(i, 1);
+            }
+        }
+
+        return urlparts[0] + (pars.length > 0 ? '?' + pars.join('&') : '');
+    }
+    return url;
+}
