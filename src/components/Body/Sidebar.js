293914689b251c61efa68fa6bc164a6db0a07ee3
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-12 19:31:59 +0300

    Only render logout button if authState is true

--- src/components/Body/Sidebar.js
+++ src/components/Body/Sidebar.js
@@ -2,12 +2,14 @@ import React, { useEffect, Fragment } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import $ from "jquery";
 
+import { selectAuthState } from "../../features/User/userSlice";
 import { selectActive, setActive } from "../../features/Subreddits/subredditsSlice";
 import SubredditList from "../../features/Subreddits/SubredditList";
 import { selectFilter, setFilter, setSearchTerm } from "../../features/Posts/postsSlice";
 
 const Sidebar = (props) => {
     const dispatch = useDispatch();
+    const authState = useSelector(selectAuthState);
     const { handleLogout } = props;
 
     const activeSub = useSelector(selectActive);
@@ -28,6 +30,27 @@ const Sidebar = (props) => {
         }, 500);
     };
 
+    const sidebarClose = () => {
+        if (authState) {
+            return (
+                <div className="sidebar-close-container">
+                    <svg className="mobile-only" id="reddit-logout" data-testid="reddit-logout" aria-label="Logout" onClick={handleLogout} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path d="M0 2v20h14v-2h-12v-16h12v-2h-14zm18 7.408l2.963 2.592-2.963 2.592v-1.592h-8v-2h8v-1.592zm-2-4.408v4h-8v6h8v4l8-7-8-7z"/>
+                    </svg>
+                    <svg className="sidebar-close" id="sidebar-close" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={closeSidebar}>
+                        <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.151 17.943l-4.143-4.102-4.117 4.159-1.833-1.833 4.104-4.157-4.162-4.119 1.833-1.833 4.155 4.102 4.106-4.16 1.849 1.849-4.1 4.141 4.157 4.104-1.849 1.849z"/>
+                    </svg>
+                </div>
+            )
+        }
+
+        return (
+            <svg className="sidebar-close" id="sidebar-close" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={closeSidebar}>
+                <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.151 17.943l-4.143-4.102-4.117 4.159-1.833-1.833 4.104-4.157-4.162-4.119 1.833-1.833 4.155 4.102 4.106-4.16 1.849 1.849-4.1 4.141 4.157 4.104-1.849 1.849z"/>
+            </svg>
+        )
+    }
+
     useEffect(() => {
         closeSidebar();
     }, [dispatch, activeSub, filter])
@@ -38,14 +61,7 @@ const Sidebar = (props) => {
                 <div>
                     <div className="sidebar-header">
                         <h2 id="sidebar-title">Subreddits</h2>
-                        <div className="sidebar-close-container">
-                            <svg className="mobile-only" id="reddit-logout" data-testid="reddit-logout" aria-label="Logout" onClick={handleLogout} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                <path d="M0 2v20h14v-2h-12v-16h12v-2h-14zm18 7.408l2.963 2.592-2.963 2.592v-1.592h-8v-2h8v-1.592zm-2-4.408v4h-8v6h8v4l8-7-8-7z"/>
-                            </svg>
-                            <svg className="sidebar-close" id="sidebar-close" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={closeSidebar}>
-                                <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.151 17.943l-4.143-4.102-4.117 4.159-1.833-1.833 4.104-4.157-4.162-4.119 1.833-1.833 4.155 4.102 4.106-4.16 1.849 1.849-4.1 4.141 4.157 4.104-1.849 1.849z"/>
-                            </svg>
-                        </div>
+                        {sidebarClose()}
                     </div>
                     <div className="search mobile">
                         <form>
